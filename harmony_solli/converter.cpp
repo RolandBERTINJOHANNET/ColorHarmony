2c3ea951126104cf82328a192e499a3b39cfe3b2
RolandBERTINJOHANNET <78405585+RolandBERTINJOHANNET@users.noreply.github.com>
2022-11-03 19:01:01 +0100

    début implémentation harmonie selon Solli et Lenz

---
+++ harmony_solli/converter.cpp
@@ -0,0 +1,266 @@
+#include <cmath>
+
+const double pi = 3.141592653589793;
+
+double gamma_expand(double value){
+	return value<=0.04045?(value/12.92)
+						:pow((value+0.055)/1.055, 2.4);
+}
+double gamma_compress(double value){
+	return value<=0.0031308?(value*12.92)
+						:pow(value*1.055, 1./2.4)-0.055;
+}
+unsigned char expand_to_byte(double value){
+	int oct = 255*value;
+	oct = oct>255?255:oct;
+	oct = oct<0?0:oct;
+    return (unsigned char)oct;
+}
+
+
+void convert_RGB_to_XYZ(unsigned char *img, double *XYZimg, int w, int h){
+	for(int i=0;i<w*h*3;i+=3){
+		double r,g,b;
+		//bring to 0,1 range
+		r=(double)img[i]/(double)255;
+		g=(double)img[i+1]/(double)255;
+		b=(double)img[i+2]/(double)255;
+		
+		//gamma expansion
+		r = gamma_expand(r);
+		g = gamma_expand(g);
+		b = gamma_expand(b);
+
+		//matrix multiplication
+		double X = 0.4124564*r +0.3575761*g +0.1804375*b;
+		double Y = 0.2126729*r  +0.7151522*g +0.0721750*b;
+		double Z = 0.0193339*r +0.1191920*g +0.9503041*b;
+
+		//output
+		XYZimg[i]=X;
+		XYZimg[i+1]=Y;
+		XYZimg[i+2]=Z;
+	}
+}
+
+void convert_XYZ_to_RGB(unsigned char *oimg, double *XYZimg, int w, int h){
+	for(int i=0;i<w*h*3;i+=3){
+		//matrix multiplication
+		double X = XYZimg[i];
+		double Y = XYZimg[i+1];
+		double Z = XYZimg[i+2];
+
+		//matrix multiplication
+		double r = 3.2406*X -1.5372*Y -0.4986*Z;
+		double g = -0.9689*X  +1.8758*Y +0.0415*Z;
+		double b = 0.0557*X -0.2040*Y +1.0570*Z;
+		
+		//gamma compression
+		r = gamma_compress(r);
+		g = gamma_compress(g);
+		b = gamma_compress(b);
+
+		//output
+		oimg[i]=expand_to_byte(r);
+		oimg[i+1]=expand_to_byte(g);
+		oimg[i+2]=expand_to_byte(b);
+	}
+}
+
+double LABfun(double value){
+	double delta = 6./29.;
+
+	double t = value;
+
+	return t>pow(delta, 3)?
+				pow(t, 1./3.):
+				(t/(3*pow(delta,2)))+(4./29.);
+}
+double LABfun_inv(double value){
+	double delta = 6./29.;
+
+	double t = value;
+
+	return t>delta?
+			pow(t, 3):
+			3.*pow(delta,2)*(t-(4./29.));
+}
+
+void convert_RGB_to_LAB(unsigned char *img, double *XYZimg, int w, int h){
+	convert_RGB_to_XYZ(img, XYZimg, w, h);
+
+	for(int i=0;i<w*h*3;i+=3){
+		double X = XYZimg[i];
+		double Y = XYZimg[i+1];
+		double Z = XYZimg[i+2];
+		
+		//transformation
+		double L = 116.*LABfun(Y/100.) - 16.;
+		double a = 500.*(LABfun(X/95.0489) - LABfun(Y/100.));
+		double b = 200.*(LABfun(Y/100.) - LABfun(Z/108.8840));
+	
+		//output
+		XYZimg[i]=L;
+		XYZimg[i+1]=a;
+		XYZimg[i+2]=b;
+	}
+}
+
+void convert_LAB_to_RGB(unsigned char *oimg, double *LABimg, int w, int h){
+
+	for(int i=0;i<w*h*3;i+=3){
+		double L = LABimg[i];
+		double a = LABimg[i+1];
+		double b = LABimg[i+2];
+		
+		//transformation
+		double X = 95.0489*LABfun_inv( ((L+16.)/116.)+(a/500.) );
+
+		double Y = 100.*LABfun_inv((L+16.)/116.);
+
+		double Z = 108.8840*LABfun_inv(  ((L+16.)/116.)-(b/200.) );
+	
+		//output into LAB but they are XYZ coords by now
+		LABimg[i]=X;
+		LABimg[i+1]=Y;
+		LABimg[i+2]=Z;
+
+	}
+	convert_XYZ_to_RGB(oimg, LABimg, w, h);
+}
+
+
+void RGB_to_LCH(int R, int G, int B, double *L, double *C, double *H){
+	//first convert rgb to XYZ
+	double r,g,b;
+	//bring to 0,1 range
+	r=(double)R/(double)255;
+	g=(double)G/(double)255;
+	b=(double)B/(double)255;
+	
+	//gamma expansion
+	r = gamma_expand(r);
+	g = gamma_expand(g);
+	b = gamma_expand(b);
+
+	//matrix multiplication
+	double X = 0.4124564*r +0.3575761*g +0.1804375*b;
+	double Y = 0.2126729*r  +0.7151522*g +0.0721750*b;
+	double Z = 0.0193339*r +0.1191920*g +0.9503041*b;
+
+	//then convert to LAB
+	double l = 116.*LABfun(Y/100.) - 16.;
+	double a = 500.*(LABfun(X/95.0489) - LABfun(Y/100.));
+	double b_ = 200.*(LABfun(Y/100.) - LABfun(Z/108.8840));
+
+	//then to LCH
+
+	double var_H = (pi+atan2(b_,a))*(360./(2.*pi));  //Quadrant by signs, turned into degrees
+
+
+	*L = l;
+	*C = sqrt(a*a+b_*b_);
+	*H = var_H;
+}
+
+void LCH_to_RGB(double l, double C, double H, int *r, int *g, int *b){
+	//convert to LAB
+	double H_rad = ((2.*pi)/360.)*H - pi;
+	
+	double L = l;
+	double a = C*cos(H_rad);
+	double b_ = C*sin(H_rad);
+
+
+	//then convert to XYZ
+	
+	//transformation
+	double X = 95.0489*LABfun_inv( ((L+16.)/116.)+(a/500.) );
+
+	double Y = 100.*LABfun_inv((L+16.)/116.);
+
+	double Z = 108.8840*LABfun_inv(  ((L+16.)/116.)-(b_/200.) );
+
+	//matrix multiplication
+	double R = 3.2406*X -1.5372*Y -0.4986*Z;
+	double G = -0.9689*X  +1.8758*Y +0.0415*Z;
+	double B = 0.0557*X -0.2040*Y +1.0570*Z;
+	
+	//gamma compression
+	R = gamma_compress(R);
+	G = gamma_compress(G);
+	B = gamma_compress(B);
+
+	//output
+	*r=expand_to_byte(R);
+	*g=expand_to_byte(G);
+	*b=expand_to_byte(B);
+}
+
+void RGB_to_LAB(double *r, double *g, double *b){
+	//first convert rgb to XYZ
+	//bring to 0,1 range
+	*r/=255.;
+	*g/=255.;
+	*b/=255.;
+	
+	//gamma expansion
+	*r = gamma_expand(*r);
+	*g = gamma_expand(*g);
+	*b = gamma_expand(*b);
+
+	//matrix multiplication
+	double X = 0.4124564*(*r) +0.3575761*(*g) +0.1804375*(*b);
+	double Y = 0.2126729*(*r)  +0.7151522*(*g) +0.0721750*(*b);
+	double Z = 0.0193339*(*r) +0.1191920*(*g) +0.9503041*(*b);
+
+	//then convert to LAB
+	*r = 116.*LABfun(Y/100.) - 16.;
+	*g = 500.*(LABfun(X/95.0489) - LABfun(Y/100.));
+	*b = 200.*(LABfun(Y/100.) - LABfun(Z/108.8840));
+}
+
+void LAB_to_RGB(double *L, double *a, double *b){
+	//convert to XYZ
+	
+	//transformation
+	double X = 95.0489*LABfun_inv( (((*L)+16.)/116.)+((*a)/500.) );
+
+	double Y = 100.*LABfun_inv(((*L)+16.)/116.);
+
+	double Z = 108.8840*LABfun_inv(  (((*L)+16.)/116.)-((*b)/200.) );
+
+	//matrix multiplication
+	double R = 3.2406*X -1.5372*Y -0.4986*Z;
+	double G = -0.9689*X  +1.8758*Y +0.0415*Z;
+	double B = 0.0557*X -0.2040*Y +1.0570*Z;
+	
+	//gamma compression
+	R = gamma_compress(R);
+	G = gamma_compress(G);
+	B = gamma_compress(B);
+
+	//output
+	*L=expand_to_byte(R);
+	*a=expand_to_byte(G);
+	*b=expand_to_byte(B);
+}
+
+void LAB_to_LCH(double *L, double *a, double *b){
+	//convert to LCH
+
+	double var_H = (pi+atan2(*b,*a))*(360./(2.*pi));  //Quadrant by signs, turned into degrees
+
+
+	*a = sqrt((*a)*(*a)+(*b)*(*b));
+	*b = var_H;
+}
+
+void LCH_to_LAB(double *L, double *a, double *b){
+	//convert to LAB
+	double C = *a;
+	double H=*b;
+	double H_rad = ((2.*pi)/360.)*H - pi;
+	*a = C*cos(H_rad);
+	*b = C*sin(H_rad);
+}
---
+++ harmony_solli/harmony_solli.cpp
@@ -0,0 +1,50 @@
+#include "converter.cpp"
+#include <math.h>
+
+double compute_deltaC(double C1, double H1, double C2, double H2){
+	return sqrt(pow(H1-H2,2) + pow((C1-C2)/1.46,2));
+}
+
+double compute_HdeltaL(double L1, double L2){
+	return 0.14 + 0.15 * tanh(-2+0.2 * abs(L1-L2));
+}
+
+double compute_Hlsum(double L1, double L2){
+	return 0.28 + 0.54 * tanh(-3.88 + 0.029 * (L1+L2));
+}
+
+double compute_Ec(double C1){
+	return 0.5 + 0.5 * tanh(-2+0.5 * C1);
+}
+
+double compute_Hs(double H1){
+	return -0.08 - 0.14 * sin(H1+50) - 0.07 * sin(2*H1 + 90);
+}
+
+double compute_Ey(double L1, double H1){
+	double operand = (90-H1)/(10.);
+	return ((0.22 * L1 - 12.8)/(10.))*exp(operand-exp(operand));
+}
+
+double compute_Hsy(double L1, double C1, double H1){
+	return compute_Ec(C1) * (compute_Hs(H1) + compute_Ey(L1,H1));
+}
+
+double Hc(double C1, double H1, double C2, double H2){
+	return 0.04 + 0.53 * tanh(0.8 - 0.045 * compute_deltaC(C1,H1,C2,H2));
+}
+double Hh(double L1, double C1, double H1, double L2, double C2, double H2){
+	return compute_Hsy(L1,C1,H1)+compute_Hsy(L2,C2,H2);
+}
+double Hl(double L1,double L2){
+	return compute_Hlsum(L1,L2) + compute_HdeltaL(L1,L2);
+}
+
+double compute_harmony(int r1, int g1, int b1, int r2, int g2, int b2){
+	double L1,C1,H1,L2,C2,H2;
+	RGB_to_LCH(r1,g1,b1,&L1,&C1,&H1);
+	RGB_to_LCH(r2,g2,b2,&L2,&C2,&H2);
+	return Hc(C1,H1,C2,H2) + 
+			Hl(L1,L2) + 
+			Hh(L1,C1,H1,L2,C2,H2);
+}
