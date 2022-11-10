6cba771e9c3e3f18f313854c6c07eb7424f1a1be
RolandBERTINJOHANNET <78405585+RolandBERTINJOHANNET@users.noreply.github.com>
2022-11-10 16:38:41 +0100

    fine tuning the kmeans harmony type method with cluster weights and conditions on LCH characteristics

--- python_scripts/harmony.py
+++ python_scripts/harmony.py
@@ -16,26 +16,25 @@ def getMainColors_kmeans(img):
 	#extract hues and select those that matter
 	hues = []
 	img = np.array(img)
-	img = np.reshape(img,(img.shape[0]*img.shape[1],3))
+	img = np.reshape(img,(img.shape[0]*img.shape[1],3))/255.
 	for pixel in np.array(img):
 		rgb = sRGBColor(pixel[0],pixel[1],pixel[2])
 		LCH_color = convert_color(rgb, LCHabColor)
 		lch = (LCH_color.lch_l,LCH_color.lch_c,LCH_color.lch_h)
-		if lch[0]>50 and lch[1]>50:
+		if lch[0]>20 and lch[1]>20:
 			hues.append(lch[2])
-			#take colors that pop more into account
-			if lch[0]>85:
-				hues.append(lch[2])
-			if lch[1]>100:
-				hues.append(lch[2])
 		
 	#kmeans
-	if(len(hues)>1):#if at least some hues are associated with enough saturation and luminance
+	if(len(hues)>500):#if enough hues are associated with enough saturation and luminance to give good estimate of color harmony
 		hues = np.reshape(np.array(hues),(len(hues),1))
 		km = KMeans(n_clusters=4).fit(hues)
 		pred = km.predict(hues)
+		#for k in range(4):
+			#print("cluster",k,"has",len(np.where(pred==k)[0]),"and center",km.cluster_centers_[k])
+		
 		#take only the sufficiently big clusters.
-		colors = [km.cluster_centers_[i] for i in range(4) if len(np.where(pred==i)[0])>((1./7.)*len(hues))]
+		colors = [km.cluster_centers_[i] for i in range(4) if len(np.where(pred==i)[0])>100]
+		#print("colors : ",colors)
 		
 		#grouping of colors
 		bins=[]
@@ -46,14 +45,16 @@ def getMainColors_kmeans(img):
 			colors.remove(hue)
 			j=0
 			while j<len(colors):
-				if abs(hue - colors[j])<50:
+				if abs(hue - colors[j])<65:
 					similar_colors.append(colors[j])
 					colors.remove(colors[j])
 				else:
 					j+=1
 			bins.append(np.mean(similar_colors))
+		#print("after similarity check : ",bins)
 		return bins
 	else:
+		print("skipped image")
 		return None
 
 
@@ -62,7 +63,7 @@ def getHarmonyType(bins):
 		if len(bins)==1:
 			return "mono"
 		elif len(bins)==2:
-			return "comp" if abs(bins[0]-bins[1])>90 else "analog"
+			return "comp" if abs(bins[0]-bins[1])>110 else "analog"
 		elif len(bins)==3:
 			return "triad"
 		else:
@@ -76,7 +77,7 @@ def draw_maincolors(img,bins):
 		rgb = [RGB_color.rgb_r,RGB_color.rgb_g,RGB_color.rgb_b]
 		for j in range(i*50,(i+1)*50):
 			for k in range(100):
-				last_line[j,k]=np.array(rgb) * 255
+				last_line[j,k]=np.array([rgb[0]*255,rgb[1]*255,rgb[2]*360])
 	final = np.concatenate([np.array(img),last_line],axis=1)
 	return Image.fromarray(final.astype(np.uint8))
 
@@ -92,11 +93,11 @@ if __name__ == "__main__":
 	for file in os.listdir(data_dir_small):
 		img = Image.open(data_dir_small+"/"+file).convert("RGB")
 		bins = getMainColors_kmeans(img)
-		print(np.array(img).shape)
 		if bins is not None:
 			harmonyType = getHarmonyType(bins)
+			#print("final choice was ",harmonyType)
 			img = Image.open(data_dir_big+"/"+file).convert("RGB")
 			img = draw_maincolors(img,bins)
-			print(i,"images done : ",out_dir+harmonyType+"/"+file)
+			print("\n",i,"images done : ",out_dir+harmonyType+"/"+file)
 			i+=1
 			img.save(out_dir+harmonyType+"/"+file)
