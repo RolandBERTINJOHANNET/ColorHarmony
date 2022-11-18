3325ce2aef910951ce3e30f3483aae051fea6b77
RolandBERTINJOHANNET <78405585+RolandBERTINJOHANNET@users.noreply.github.com>
2022-11-18 19:31:27 +0100

    made the final database with the 6th class, + prepared the MTURK dataset for MLE color-pair harmony estimation

--- harmony_yang/extract_data_from_mat.py
+++ harmony_yang/extract_data_from_mat.py
@@ -8,3 +8,24 @@ palettes = np.array(mat["data"])
 scores = np.array(mat["targets"])
 
 print("shape of scores : ", scores.shape, "shape of palettes : ", palettes.shape)
+print("first line of scores : ",scores[0:5])
+print("first line of palettes : ",palettes[0,0,:])
+
+#reshape to fit into a csv file
+palettes = palettes.reshape((palettes.shape[0],-1))
+scores = scores.reshape((scores.shape[0],-1))
+
+#save to file
+np.savetxt("palettes.csv", palettes, delimiter=",")
+np.savetxt("scores.csv", scores, delimiter=",")
+
+#verify they can be loaded back
+palettes_shape = np.array(mat["data"]).shape
+scores_shape = np.array(mat["targets"]).shape
+
+palettes = np.loadtxt("palettes.csv", delimiter=",").reshape(palettes_shape)
+scores = np.loadtxt("scores.csv", delimiter=",").reshape(scores_shape)
+
+print("shape of scores from files: ", scores.shape, "shape of palettes from files: ", palettes.shape)
+print("first line of scores : ",scores[0:5])
+print("first line of palettes : ",palettes[0,0,:])
