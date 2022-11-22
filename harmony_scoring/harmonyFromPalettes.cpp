a54802d96cfd6eac7aa5f4c006121d01d04201de
RolandBERTINJOHANNET <78405585+RolandBERTINJOHANNET@users.noreply.github.com>
2022-11-22 11:31:50 +0100

    fini extrapolation à palettes + lancé experimentation

--- harmony_scoring/harmonyFromPalettes.cpp
+++ harmony_scoring/harmonyFromPalettes.cpp
@@ -1,42 +1,60 @@
 #include <algorithm>
+#include <iterator>
 //for the solli harmony prediction
 #include "../harmony_scoring/harmony_solli/harmony_solli.cpp"
 //for the yang harmony prediction
 #include "../split_merge/utils.cpp"
 
-//!!!!!!!!!!!!!!!!!!!!!!!!for solli prediction, since only the hues are known in the palettes, we're doing 
-//palette will either be rgb or hues, based on use_solli (rgb if True, hues if False)
+//-------------------------------------------------------------------------------------------------------------------
+//what the code does : 
+//reads all the data from the Kuler dataset (harmony_yang/palettesKuler.csv and harmony_yang/scoresKuler.csv)
+//makes a harmony prediction from the 4 pairs in each palette,
+									//one prediction for the yang et al. and one for renz et al. method 
+//writes these predictions to a .csv file for further processing down the line.
+//-------------------------------------------------------------------------------------------------------------------
+
+
 //CSVDatat is only used if use_solli is false
-float prediction_from_palette(std::vector<int> palette, std::vector<float> CSVData, bool use_solli){
+float prediction_from_palette(std::vector<float> palette, std::vector<float> CSVData, bool use_solli){
 	float MLEScore = 0.;
 	if(use_solli){
-		std::cout<<"using solli et al. harmony \nsize of palette : "<<palette.size()<<std::endl;
+		//std::cout<<"using solli et al. harmony \nsize of palette : "<<palette.size()<<std::endl;
 		for(int i=0;i<palette.size()-3;i+=3){
 			int r1,g1,b1,r2,g2,b2;
-			r1=palette[i];g1=palette[i+1];b1=palette[i+2];
-			r2=palette[i*3];g2=palette[i*3+1];b2=palette[i*3+2];
+			r1=palette[i]*255;g1=palette[i+1]*255;b1=palette[i+2]*255;
+			r2=palette[i+3]*255;g2=palette[i+4]*255;b2=palette[i+5]*255;
 
-			std::cout<<"the two colors : "<<r1<<", "<<g1<<", "<<b1<<", "<<r2<<", "<<g2<<", "<<b2<<", "<<std::endl;
+			//std::cout<<"the two colors : "<<r1<<", "<<g1<<", "<<b1<<", "<<r2<<", "<<g2<<", "<<b2<<", "<<std::endl;
 
 			float pairPrediction = compute_harmony(r1,g1,b1,r2,g2,b2);
+			MLEScore+=pairPrediction;
 			std::cout<<"solli pair prediction : "<<pairPrediction<<std::endl;
 		}
 
 	}else{
-		std::cout<<"using yang et al. harmony \nsize of palette : "<<palette.size()<<std::endl;
-		for(int i=0;i<palette.size()-1;i+=3){
-			float h1,h2;
-			h1=palette[i];h2=palette[i+1];//ignore the score (which is at i+2)
+		//std::cout<<"using yang et al. harmony \nsize of palette : "<<palette.size()<<std::endl;
+		for(int i=0;i<palette.size()-3;i+=3){
+			float r1,g1,b1,r2,g2,b2;
+			double l,c,h,l1,c1,h1;
 
-			std::cout<<"the two hues : "<<h1<<", "<<h2<<std::endl;
+			r1=palette[i]*255;g1=palette[i+1]*255;b1=palette[i+2]*255;
+			r2=palette[i+3]*255;g2=palette[i+4]*255;b2=palette[i+5]*255;
 
-			float pairPrediction = predictionFromCSV(h1, h2, CSVData);
-			std::cout<<"pair prediction : "<<pairPrediction<<std::endl;
+
+			//std::cout<<"the two colors : "<<r1<<", "<<g1<<", "<<b1<<", "<<r2<<", "<<g2<<", "<<b2<<", "<<std::endl;
+
+			RGB_to_LCH(r1,g1,b1,&l,&c,&h);
+			RGB_to_LCH(r2,g2,b2,&l1,&c1,&h1);
+
+
+			float pairPrediction = predictionFromCSV(h, h1, CSVData);
+			MLEScore+=pairPrediction;
+			std::cout<<"yang pair prediction : "<<pairPrediction<<std::endl;
 		}
 	}
 
-	std::cout<<"use_solli : "<<use_solli<<" final score prediction : "<<MLEScore/5.<<std::endl;
-	return MLEScore/5.;//the palette length will never not be 5 colors.
+	std::cout<<"using "<<(use_solli?"solli, ":"yang, ")<<" final score prediction : "<<MLEScore/5.<<std::endl;
+	return MLEScore/4.;//the palette length will never not be 5 colors.
 }
 
 //specifically for reading the palettes.csv file
@@ -54,7 +72,7 @@ std::vector<float> readPalettes(std::string filename){
           std::stringstream str(line);
           float tempFloat;
           str>>tempFloat;
-        	std::cout<<"read "<<line<<std::endl;
+        	//std::cout<<"read "<<tempFloat*255.<<std::endl;
           result.push_back(tempFloat);
         }else break;
       }else{
@@ -62,7 +80,7 @@ std::vector<float> readPalettes(std::string filename){
           std::stringstream str(line);
           float tempFloat;
           str>>tempFloat;
-        	std::cout<<"read "<<line<<std::endl;
+        	//std::cout<<"read "<<tempFloat*255.<<std::endl;
           result.push_back(tempFloat);
         }else break;
       }
@@ -73,10 +91,7 @@ std::vector<float> readPalettes(std::string filename){
 }
 
 
-//reads all the data from the Kuler dataset (harmony_yang/palettesKuler.csv and harmony_yang/scoresKuler.csv)
-//makes a harmony prediction from the 4 pairs in each palette,
-									//one prediction for the yang et al. and one for renz et al. method 
-//writes these predictions to a .csv file for further processing down the line.
+
 int main(){
 	//the csv file for yang et al. is "../harmony_scoring/harmony_yang/pairPlusPrediction.csv"
 	//the csv file for the RGB palettes is "../harmony_scoring/harmony_yang/palettes.csv"
@@ -85,25 +100,48 @@ int main(){
 	std::vector<float> RGBPalettes = readPalettes("../harmony_scoring/harmony_yang/palettes.csv");
 	
 	//writing the computed palette scores to "harmony_yang/paletteScoresKuler.csv" and "harmony_solli/paletteScoresKuler.csv"
-	std::string pathSol = "harmony_yang/paletteScoresKuler.csv";
+	std::string pathSol = "harmony_solli/paletteScoresKuler.csv";
 	std::string pathYan = "harmony_yang/paletteScoresKuler.csv";
 	std::ofstream fYan,fSol;
     fYan.open(pathYan,std::ios::app);
     fSol.open(pathSol,std::ios::app);
 
-//--------------:::::::::::::!!!!!!!!!!!!!!!!!!!!!!!!!!!!TROUBLE WITH ITERATORS, TRYING TO GET THE PALETTE AS A STD::VECTOR.
 
-	for(int i=0;i<15;i++){
-		auto paletteStart = RGBPalettes.begin();
-		auto paletteEnd = RGBPalettes.begin();
-		paletteStart.next(i);
-		paletteEnd.next(i+15);
-		std::vector<float> palette = std::copy(paletteStart,paletteEnd);
-		std::cout<<"palette length : "<<paletteEnd-paletteStart<<std::endl;
-		float harmony_yang = prediction_from_palette(palette, CSVData, False);
-		float harmony_solli = prediction_from_palette(palette, CSVData, True);
+		//first compute solli et al harmony on all palettes
+  std::vector<float>::iterator paletteStart,paletteEnd;
+  paletteStart = RGBPalettes.begin();
+  paletteEnd = std::next(RGBPalettes.begin(),15);
 
+	while(paletteEnd<=RGBPalettes.end()){
+		std::vector<float> palette(paletteEnd - paletteStart);
+		std::copy(paletteStart,paletteEnd,palette.begin());
+		float harmony_solli = prediction_from_palette(palette, CSVData, true);
+
+		//write to fSol
+		fSol<<harmony_solli<<std::endl;
+
+		paletteStart = std::next(paletteStart,15);
+		paletteEnd = std::next(paletteEnd,15);
 	}
 
+	//then yang on all palettes.
+	paletteStart = RGBPalettes.begin();
+  paletteEnd = std::next(RGBPalettes.begin(),15);
+
+	while(paletteEnd<=RGBPalettes.end()){
+		std::vector<float> palette(paletteEnd - paletteStart);
+		std::copy(paletteStart,paletteEnd,palette.begin());
+		float harmony_yang = prediction_from_palette(palette, CSVData, false);
+
+		//write to fYan
+		fYan<<harmony_yang<<std::endl;
+
+		paletteStart = std::next(paletteStart,15);
+		paletteEnd = std::next(paletteEnd,15);
+	}
+
+
+	fSol.close();
+	fYan.close();
 	return 1;
 }
